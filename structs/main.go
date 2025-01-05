6321e9a6fbbb3dd0b531314f91955b4897d2cab6
Sourya Roy <94044624+dangbros@users.noreply.github.com>
2025-01-04 23:47:24 -0500

    learnt about reflections a bit

--- structs/main.go
+++ structs/main.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+type Person struct {
+	Name  string `validate:"min=2,max=32"`
+	Email string `validate:"required,email"`
+}
 
 func main() {
 	fmt.Println("Structs in golang")
@@ -9,6 +17,15 @@ func main() {
 	fmt.Println(sourya)
 	fmt.Printf("Details: %+v\n", sourya)
 	fmt.Printf("Name is %v and email is %v.\n", sourya.Name, sourya.Email)
+
+	rick := User{
+		Name:  "Rick",
+		Email: "royshourya30@proton.me",
+	}
+
+	t := reflect.TypeOf(rick)
+	fmt.Println("Name: ", t.Name())
+	fmt.Println("Kind: ", t.Kind())
 }
 
 type User struct {
