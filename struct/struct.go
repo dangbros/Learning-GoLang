2fbece4d12962bc9b755a35ac9e25dcbbf072ef9
Sourya Roy <souryaroy30@gmail.com>
2025-12-05 17:07:22 +0530

    learnt struct

--- struct/struct.go
+++ struct/struct.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// this is how we created the struct
 type order struct {
 	id        string
 	amount    float32
@@ -12,25 +13,48 @@ type order struct {
 	createdAt time.Time
 }
 
+// this is the constructor
+func newOrder(id string, amount float32, status string) *order {
+	myOrder := order{
+		id:     id,
+		amount: amount,
+		status: status,
+	}
+
+	return &myOrder // we return the address of the constructor
+}
+
+// this is function of the struct
 func (o *order) updateStatus(status string) {
 	o.status = status
 }
 
+// this is also the function of the struct
 func (o order) getAmount() float32 {
 	return o.amount
 }
 
 func main() {
-	myOrder := order{
-		id:     "1",
-		amount: 50,
-		status: "received",
-	}
+	//this is how we can create a struct and instantiate and assign values
+	language := struct {
+		name   string
+		isGood bool
+	}{"golang", true}
 
-	myOrder.updateStatus("returned")
-	fmt.Println(myOrder)
+	fmt.Println(language)
+	// myOrder := newOrder("1", 50, "received")
+	// fmt.Println(myOrder.amount)
 
-	fmt.Println(myOrder.getAmount())
+	// myOrder := order{
+	// 	id:     "1",
+	// 	amount: 50,
+	// 	status: "received",
+	// }
+
+	// myOrder.updateStatus("returned")
+	// fmt.Println(myOrder)
+
+	// fmt.Println(myOrder.getAmount())
 
 	// myOrder.createdAt = time.Now()
 
